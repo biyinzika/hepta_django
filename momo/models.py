415731c6994529dad405bbe8de7665bf24dd165c
Benjamin Senyonyi <bejose2@yahoo.com>
2019-08-13 18:12:25 +0300

    Updates to DB and payment accounts

--- momo/models.py
+++ momo/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.utils import timezone
-
-
+from momoapi import *
+# import httplib, urllib
+# import http, urllib.request, urllib.parse, urllib.error
+# import urllib, base64, http.client
 # Create your models here.
 
 class MomoRequest(models.Model):
+    '''
+    MomoRequest model for storing requests as request_text
+    '''
     request_text = models.CharField(max_length=240)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=1, max_digits=10000)
@@ -13,7 +18,14 @@ class MomoRequest(models.Model):
     def __str__(self):
         return self.request_text
 
+
 class CollectionRequest(models.Model):
+    '''
+    Collection request model
+    Trigger a collection_request whenever a new MomoRequest object is created (foreign key as momo_request)
+    Status of payment either pending or paid as in choices.
+    
+    '''
 #     collection request trigger
     collection_request = models.BooleanField(default=True)
     poll = models.IntegerField(default=0)
@@ -21,19 +33,23 @@ class CollectionRequest(models.Model):
     momo_request = models.ForeignKey(MomoRequest, on_delete=models.CASCADE)
     
 #     Status of payments to be updated in Momo Request
-#     PENDING = 'Pending'
-#     PAID = 'Paid'
-#     STATUS_CHOICES = (
-#         (PENDING, 'Pending'),
-#         (PAID, 'Paid')
-#     )
+    PENDING = 'Pending'
+    PAID = 'Paid'
+    STATUS_CHOICES = (
+        (PENDING, 'Pending'),
+        (PAID, 'Paid')
+    )
 #     payment account type
-#     payment_level = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=PENDING)
+    payment_level = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=PENDING)
     
     def __str__(self):
         return self.poll
 
 class Personal_Account(models.Model):
+    '''
+    Personal account model storing usernames as unique values
+    
+    '''
     name = models.CharField(max_length=200)
     username = models.CharField(unique=True, max_length=20)
     account_number = models.IntegerField(null=False)
@@ -47,10 +63,33 @@ class Personal_Account(models.Model):
         return self.username
 
     def delete(self):
-        """
-        Don't delete the Account
-        Instead just set `is_active` to False
-        """
+        '''
+        Account not deleted but changes is_active to False
+        '''
         self.is_active = False
         self.save()
         return self
+    
+
+# class Collection(models.Model):
+#     headers = {
+#     # Request headers
+#     'X-Reference-Id': '',
+#     'Content-Type': 'application/json',
+#     'Ocp-Apim-Subscription-Key': '{subscription key}',
+#     }
+#      
+#     params = urllib.parse.urlencode({
+#     })
+#      
+#     try:
+#         conn = http.client.HTTPSConnection('ericssonbasicapi2.azure-api.net')
+#         conn.request("POST", "/v1_0/apiuser?%s" % params, "{body}", headers)
+#         response = conn.getresponse()
+#         data = response.read()
+#         print(data)
+#         conn.close()
+#     except Exception as e:
+#         print("[Errno {0}] {1}".format(e.errno, e.strerror))
+#         
+        
